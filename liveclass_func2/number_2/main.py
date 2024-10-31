aeca11805838d4fb3d1789ddd1d5fba26a67faa1
aisyahvasthi <aisyah.vasthiwinona@gmail.com>
2024-10-31 20:24:31 +0700

    edit print

--- liveclass_func2/number_2/main.py
+++ liveclass_func2/number_2/main.py
@@ -1,4 +1,4 @@
 import perhitungan_angsuran_per_bulan_klien as angsuran_per_bulan
 
 angsuran = angsuran_per_bulan.bunga_flat(10000000, 10)
-print(f'Angsuran perbulan yang wajib dibayarkan Rp. {round(angsuran)}')
+print(f'Angsuran perbulan yang wajib dibayar Rp. {round(angsuran)}')
